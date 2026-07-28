f22db34cb3fc804a02f026734fc9db755149fae1
Meghashyam Chevur <meghashyamexp@gmail.com>
2026-07-28 21:14:17 +0530

    feat(intelligence): add company intelligence model

--- ici/models/__init__.py
+++ ici/models/__init__.py
@@ -1,5 +1,11 @@
 """Domain models for the ICI package."""
 
 from .company import Company
+from .company_financials import CompanyFinancials
+from .company_intelligence import CompanyIntelligence
 
-__all__ = ["Company"]
+__all__ = [
+    "Company",
+    "CompanyFinancials",
+    "CompanyIntelligence",
+]

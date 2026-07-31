70bc14370112a7569e82209629d31c225a62292e
Meghashyam Chevur <meghashyamexp@gmail.com>
2026-07-31 21:38:44 +0530

    Sprint 14: implement investment decision engine

---
+++ ici/constants/investment.py
@@ -0,0 +1,20 @@
+"""Investment analysis constants."""
+
+# ----------------------------------------------------------------------
+# Investment Score Weights
+# ----------------------------------------------------------------------
+
+QUALITY_WEIGHT = 0.60
+FINANCIAL_STRENGTH_WEIGHT = 0.30
+SHAREHOLDER_RETURN_WEIGHT = 0.10
+
+# ----------------------------------------------------------------------
+# Investment Thresholds
+# ----------------------------------------------------------------------
+
+TARGET_ROE = 20.0
+TARGET_ROCE = 20.0
+TARGET_OPERATING_MARGIN = 25.0
+TARGET_PROMOTER_HOLDING = 60.0
+TARGET_DEBT_TO_EQUITY = 0.50
+TARGET_DIVIDEND_YIELD = 2.0
